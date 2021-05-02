84366384dcfc19cf363ac8ba869600021dc03fef
Yash728 <61221867+Yash728@users.noreply.github.com>
2021-05-02 19:54:09 +0530

    api created

--- firstServer.js
+++ firstServer.js
@@ -1,5 +1,9 @@
 var http = require('http');
+var data = [{ name: 'Yash', age: "20", email: "yashsv51@gmail.com" },
+{ name: 'Mohit', age: "20", email: "Mohit@gmail.com" },
+{ name: 'Sagar', age: "20", email: "Sagar@gmail.com" }]
 http.createServer((req, res) => {
-    res.write("Hello world");
+    res.writeHead(200, { 'Content-Type': 'application\json' })
+    res.write(JSON.stringify(data));
     res.end();
 }).listen(3000)
