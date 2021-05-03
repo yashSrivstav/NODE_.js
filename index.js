1f86db54458a5553daff6a911d16a793c9aefb89
Yash728 <61221867+Yash728@users.noreply.github.com>
2021-05-03 16:03:14 +0530

    using ejs to make it dynamic

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 const express = require('express')
-const app = express();
-const checkUrl = require('./js/middleware')
-const router = express.Router()
-
-// app.use(checkUrl) global
-
-app.get('/', (req, res) => {
-    // res.send("This is HomePage")
-    res.sendFile(__dirname + '/html/home.html')
+const app = express()
+app.set('view engine', 'ejs')
+app.get("/profile/:name", (req, res) => {
+    console.log(req.params.name)
+    res.render('profile', { name: req.params.name })
 })
 
-app.get('/about', (req, res) => {
-    // res.send("This is about page")
-    res.sendFile(__dirname + '/html/about.html')
-})
-
-router.get('/login', checkUrl, (req, res) => {
-    res.sendFile(__dirname + '/html/login.html')
-})
-router.get('/home', checkUrl, (req, res) => {
-    res.send("This is login page")
-})
-app.get('/yes', (req, res) => {
-    res.send("This is login page")
-})
-app.use('/', router)
 app.listen(3000)
