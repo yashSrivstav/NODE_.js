f411b4fb8505c09f40e2b39410a76fb526a82b1d
Yash728 <61221867+Yash728@users.noreply.github.com>
2021-05-03 07:57:45 +0530

    routing techniques

--- js/Express.js
+++ js/Express.js
@@ -2,5 +2,15 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
-}).listen(3000)
+    res.send("This is HomePage")
+})
+
+app.get('/about', (req, res) => {
+    res.send("This is about page")
+})
+
+app.post('/login', (req, res) => {
+    res.send("This is login page")
+})
+
+app.listen(3000)
