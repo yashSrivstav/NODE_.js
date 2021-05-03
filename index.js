82ea306e72916c41f0332e80ad45e8743e3a70e9
Yash728 <61221867+Yash728@users.noreply.github.com>
2021-05-03 16:14:03 +0530

    Looping in ejs

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 app.get("/profile/:name", (req, res) => {
-    console.log(req.params.name)
-    res.render('profile', { name: req.params.name })
+    // console.log(req.params.name)
+    data = { email: "yash@gmail.com", address: "renukoot", skills: ["node js", "sql", "linux"] }
+    res.render('profile', { name: req.params.name, data: data })
 })
 
 app.listen(3000)
